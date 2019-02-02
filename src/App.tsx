8d3114c03d506f743bf2fae45e98a1c079dc87e4
takuyahara <h.taku86@gmail.com>
2019-02-03 08:20:57 +0900

    Add: TempoSelector

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { Component, createRef, RefObject } from 'react';
 import _ from 'lodash';
 import style from './App.module.scss';
-import Tempo from './Tempo/Tempo';
+import TempoSelector from './Tempo/TempoSelector';
 
 interface IProps {
   // empty
@@ -12,7 +12,7 @@ interface IState {
 
 class App extends Component<IProps, IState> {
   // Initialize in init()
-  // empty
+  private isDescEnabled!: boolean;
 
   // Ref
   // empty
@@ -28,13 +28,7 @@ class App extends Component<IProps, IState> {
     // empty
 }
   private init(props: IProps): void {
-    // empty
-  }
-  public componentDidMount(): void {
-    const root = document.getElementById('root')!;
-    root.addEventListener('touchstart', e => {
-      e.preventDefault();
-    });
+    this.isDescEnabled = true;
   }
   public componentWillReceiveProps(newProps: IProps): void {
     if (_.isEqual(this.props, newProps)) {
@@ -46,14 +40,17 @@ class App extends Component<IProps, IState> {
   public render(): React.ReactNode {
     return (
       <div className={style.App}>
-        <Tempo 
-          role="from" 
-          tempo={80} 
-          range={{
-            from: 50,
-            to: 200,
+        <TempoSelector 
+          defaultTempo={{
+            from: 90,
+            to: 160,
           }} 
-          maxDelta={100} 
+          range={{
+            from: 60,
+            to: 780,
+          }} 
+          maxDelta={100}
+          isDescEnabled={this.isDescEnabled} 
         />
       </div>
     );
--- stories/index.stories.tsx
+++ stories/index.stories.tsx
@@ -2,11 +2,42 @@ import React, { createRef, RefObject } from 'react';
 import { storiesOf } from '@storybook/react';
 import { button, number, boolean } from '@storybook/addon-knobs';
 
+import TempoSelector from 'Tempo/TempoSelector';
 import Tempo from 'Tempo/Tempo';
 
+const refTempoSelector: RefObject<TempoSelector> = createRef<TempoSelector>();
 const refTempo: RefObject<Tempo> = createRef<Tempo>();
 
 storiesOf('Tempo', module)
+  .add('selector', () => {
+      const defaultTempo = {
+        from: number('Default tempo from', 90),
+        to: number('Default tempo to', 120),
+      };
+      const range = {
+        from: number('Range from', 50),
+        to: number('Range to', 200),
+      };
+      const maxDelta = number('Max delta', 100);
+      const isHandlerEnabled = boolean('isHandlerEnabled', true);
+      const isDescEnabled = boolean('isDescEnabled', false);
+      button('getChildrenTempo()', () => {
+        console.log(refTempoSelector.current!.getChildrenTempo());
+      });
+      return (
+        <TempoSelector 
+          ref={refTempoSelector} 
+          defaultTempo={defaultTempo} 
+          range={range} 
+          maxDelta={maxDelta} 
+          isHandlerEnabled={isHandlerEnabled} 
+          isDescEnabled={isDescEnabled} 
+        />
+      );
+    }, {
+      notes: `Buttons to set beginning/final tempo.`,
+    },
+  )
   .add('from', () => {
       const tempo = number('Tempo', 80);
       const range = {
