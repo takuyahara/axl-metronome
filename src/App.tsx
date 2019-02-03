75a4f56b69f42a0528f9d892a86bcc2582cf029b
takuyahara <h.taku86@gmail.com>
2019-02-03 15:33:56 +0900

    Add: TempoSelector

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React, { Component, createRef, RefObject } from 'react';
 import _ from 'lodash';
 import style from './App.module.scss';
-import Tempo from './Tempo/Tempo';
+import TempoSelector from './Tempo/TempoSelector';
+import * as configcat from "configcat-node";
+const configCatClient = configcat.createClient("PIjWCOBb9OZa_oaa7Igmyg/Ijc1BKL_IEy-rRjFtmCvGg");
+const myUser = { 
+  identifier: "435170f4-8a8b-4b67-a723-505ac7cdea92",
+};
 
 interface IProps {
   // empty
@@ -12,7 +17,7 @@ interface IState {
 
 class App extends Component<IProps, IState> {
   // Initialize in init()
-  // empty
+  private isDescEnabled!: boolean;
 
   // Ref
   // empty
@@ -28,7 +33,11 @@ class App extends Component<IProps, IState> {
     // empty
 }
   private init(props: IProps): void {
-    // empty
+    this.isDescEnabled = true;
+    configCatClient.getValue("isDescEnabled", this.isDescEnabled, (value: boolean) => {
+      this.isDescEnabled = value;
+      this.forceUpdate();
+    }, myUser);
   }
   public componentWillReceiveProps(newProps: IProps): void {
     if (_.isEqual(this.props, newProps)) {
@@ -40,14 +49,17 @@ class App extends Component<IProps, IState> {
   public render(): React.ReactNode {
     return (
       <div className={style.App}>
-        <Tempo 
-          role="from" 
-          tempo={80} 
-          range={{
-            from: 50,
-            to: 200,
+        <TempoSelector 
+          defaultTempo={{
+            from: 90,
+            to: 160,
           }} 
-          maxDelta={100} 
+          range={{
+            from: 60,
+            to: 780,
+          }} 
+          maxDelta={100}
+          isDescEnabled={this.isDescEnabled} 
         />
       </div>
     );
